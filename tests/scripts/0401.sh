cc44938d79ace5736dd1c924d33b683f3632fb01
Niklas Cassel <niklas.cassel@wdc.com>
2023-01-03 11:24:19 +0100

    tests: add non-NCQ tests as separate tests
    Add non-NCQ tests as separate tests.
    That way, we can test everything in the same test run.
    
    Tests created using:
    for file in 03*.sh
            do a=${file%.sh} && a=${a:1} a=$((a+100)) && cp -- "$file" 0$a.sh
    done
    sed -i 's/ncq=1/ncq=0/' 04*.sh
    sed -i 's/testname="\(.*\)"/testname="\1 ncq=off"/' 04*.sh
    
    Signed-off-by: Niklas Cassel <niklas.cassel@wdc.com>

---
+++ tests/scripts/0401.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# SPDX-License-Identifier: GPL-2.0-or-later
+#
+# Copyright (C) 2022 Western Digital Corporation or its affiliates.
+#
+
+. "${scriptdir}/test_lib"
+
+testname="CDL dur. guideline (0x1 continue-next-limit policy) ncq=off"
+T2A_file="${scriptdir}/cdl/T2A-duration-guideline.cdl"
+T2B_file="${scriptdir}/cdl/T2B-empty.cdl"
+cdl_dld=2
+expect_error=0
+compare_latencies=1
+ncq=0
+
+if [ $# == 0 ]; then
+	echo $testname
+	exit 0
+fi
+
+filename=$0
+dev=$1
+
+# Check if supported
+have_dg="$(cdladm info "$1" | grep -c "Command duration guidelines: supported")"
+if [ "${have_dg}" == "0" ]; then
+	exit_skip
+fi
+
+execute_test "$testname" $T2A_file $T2B_file $cdl_dld $expect_error $compare_latencies $filename $dev $ncq || \
+	exit_failed " --> FAILED (error executing test)"
+
+exit 0
---
+++ tests/scripts/0413.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# SPDX-License-Identifier: GPL-2.0-or-later
+#
+# Copyright (C) 2022 Western Digital Corporation or its affiliates.
+#
+
+. "${scriptdir}/test_lib"
+
+testname="CDL active time (0xf abort policy) ncq=off"
+T2A_file="${scriptdir}/cdl/T2A-active-time.cdl"
+T2B_file="${scriptdir}/cdl/T2B-empty.cdl"
+cdl_dld=4
+expect_error=1
+compare_latencies=0
+ncq=0
+
+if [ $# == 0 ]; then
+	echo $testname
+	exit 0
+fi
+
+filename=$0
+dev=$1
+
+execute_test "$testname" $T2A_file $T2B_file $cdl_dld $expect_error $compare_latencies $filename $dev $ncq || \
+	exit_failed " --> FAILED (error executing test)"
+
+exit 0
