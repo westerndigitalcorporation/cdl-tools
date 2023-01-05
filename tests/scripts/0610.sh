54f3335e1c73eadf95754233ad868b2af7c79c27
Niklas Cassel <niklas.cassel@wdc.com>
2023-01-05 14:11:48 +0100

    tests: add non-NCQ write tests
    Tests created using:
    for file in 05*.sh
    	do a=${file%.sh} && a=${a:1} a=$((a+100)) && cp -- "$file" 0$a.sh
    done
    sed -i 's/ncq=1/ncq=0/' 06*.sh
    sed -i 's/testname="\(.*\)"/testname="\1 ncq=off"/' 06*.sh
    
    Signed-off-by: Niklas Cassel <niklas.cassel@wdc.com>

---
+++ tests/scripts/0610.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# SPDX-License-Identifier: GPL-2.0-or-later
+#
+# Copyright (C) 2022 Western Digital Corporation or its affiliates.
+#
+
+. "${scriptdir}/test_lib"
+
+testname="CDL active time (0x0 complete-earliest policy) writes ncq=off"
+T2A_file="${scriptdir}/cdl/T2A-empty.cdl"
+T2B_file="${scriptdir}/cdl/T2B-active-time.cdl"
+cdl_dld=1
+expect_error=0
+compare_latencies=0
+ncq=0
+rw=randwrite
+
+if [ $# == 0 ]; then
+	echo $testname
+	exit 0
+fi
+
+filename=$0
+dev=$1
+
+execute_test "$testname" $T2A_file $T2B_file $cdl_dld $expect_error $compare_latencies $filename $dev $ncq $rw || \
+	exit_failed " --> FAILED (error executing test)"
+
+exit 0
