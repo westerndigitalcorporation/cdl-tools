dd98124ac58017fbe642336861322f8595b4e262
Niklas Cassel <niklas.cassel@wdc.com>
2023-01-05 13:26:17 +0100

    tests: add NCQ write tests
    Tests created using:
    for file in 03*.sh
    	do a=${file%.sh} && a=${a:1} a=$((a+200)) && cp -- "$file" 0$a.sh
    done
    sed -ri 's/testname="(.*) reads"/testname="\1 writes"/' 05*.sh
    sed -ri 's/rw=randread/rw=randwrite/' 05*.sh
    sed -ri 's,T2A_file=(.+),T2A_file="${scriptdir}/cdl/T2A-empty.cdl"\nT2B_file=\1,' 05*.sh
    sed -ri '/T2B_file=.+T2B-empty.cdl"/d' 05*.sh
    sed -ri 's,T2B_file=(.+)T2A(.+),T2B_file=\1T2B\2,' 05*.sh
    
    Signed-off-by: Niklas Cassel <niklas.cassel@wdc.com>

---
+++ tests/scripts/0503.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+# SPDX-License-Identifier: GPL-2.0-or-later
+#
+# Copyright (C) 2022 Western Digital Corporation or its affiliates.
+#
+
+. "${scriptdir}/test_lib"
+
+testname="CDL dur. guideline (0xd complete-unavailable policy) writes"
+T2A_file="${scriptdir}/cdl/T2A-empty.cdl"
+T2B_file="${scriptdir}/cdl/T2B-duration-guideline.cdl"
+cdl_dld=5
+expect_error=1
+compare_latencies=0
+ncq=1
+rw=randwrite
+
+if [ $# == 0 ]; then
+	echo $testname
+	exit 0
+fi
+
+filename=$0
+dev=$1
+
+if dev_has_bad_fw "$1"; then
+	exit_skip
+fi
+
+# Check if supported
+have_dg="$(cdladm info "$1" | grep -c "Command duration guidelines: supported")"
+if [ "${have_dg}" == "0" ]; then
+	exit_skip
+fi
+
+execute_test "$testname" $T2A_file $T2B_file $cdl_dld $expect_error $compare_latencies $filename $dev $ncq $rw || \
+	exit_failed " --> FAILED (error executing test)"
+
+exit 0
---
+++ tests/scripts/0512.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+# SPDX-License-Identifier: GPL-2.0-or-later
+#
+# Copyright (C) 2022 Western Digital Corporation or its affiliates.
+#
+
+. "${scriptdir}/test_lib"
+
+testname="CDL active time (0xe abort-recovery policy) writes"
+T2A_file="${scriptdir}/cdl/T2A-empty.cdl"
+T2B_file="${scriptdir}/cdl/T2B-active-time.cdl"
+cdl_dld=3
+expect_error=1
+compare_latencies=0
+ncq=1
+rw=randwrite
+
+if [ $# == 0 ]; then
+	echo $testname
+	exit 0
+fi
+
+filename=$0
+dev=$1
+
+# While ACTIVE TIME LIMIT POLICY with value 0xE is valid for SCSI.
+# ACTIVE TIME LIMIT POLICY with value 0xE is reserved for ATA.
+if dev_is_ata "$dev"; then
+	exit_skip
+fi
+
+execute_test "$testname" $T2A_file $T2B_file $cdl_dld $expect_error $compare_latencies $filename $dev $ncq $rw || \
+	exit_failed " --> FAILED (error executing test)"
+
+exit 0
