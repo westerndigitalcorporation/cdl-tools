1e299bbe6844472b6149f14eecfeeef061e64c1d
Damien Le Moal <damien.lemoal@opensource.wdc.com>
2023-03-01 15:59:11 +0900

    tests: Update to support new CDL API
    Update the test library and test case 0200 to support the new CDL API
    which passes an IO limit index using IO priority hints.
    
    Signed-off-by: Damien Le Moal <damien.lemoal@opensource.wdc.com>

--- tests/scripts/0200.sh
+++ tests/scripts/0200.sh
@@ -15,83 +15,19 @@ fi
 dev="$(realpath $1)"
 bdev="$(basename ${dev})"
 
-function check_rw()
-{
-	local rw="$(basename "$1")"
+# Check for the presence of the cdl attributes
+sysfsdir="/sys/block/${bdev}/device"
 
-	echo "Checking $1"
+echo "Checking ${sysfsdir}/cdl_supported"
 
-	if [ ! -d "$1" ]; then
-		exit_failed " --> FAILED (missing ${rw} directory)"
-	fi
-
-	if [ ! -f "$1/page" ]; then
-		exit_failed " --> FAILED (missing ${rw} page attribute)"
-	fi
-
-	if [ "${rw}" == "read" ]; then
-		type="READ"
-	else
-		type="WRITE"
-	fi
-
-	cdlp="$(cat "$1/page")"
-	str="$(cdladm list "${dev}" | grep "${type}")"
-	types=(${str})
-
-	if [ "${cdlp}" != "${types[0]}" ]; then
-		exit_failed " --> FAILED (cdladm and sysfs page types differ for $1)"
-	fi
-}
-
-function check_desc()
-{
-	local rw="$(basename "$1")"
-
-	dir="$1/$2"
-	if [ ! -d "${dir}" ]; then
-		exit_failed " --> FAILED (missing directory for ${rw} descriptor $2)"
-	fi
-
-	echo "Checking ${dir}"
-
-	attrs=("duration_guideline" \
-		"duration_guideline_policy" \
-		"max_active_time" \
-		"max_active_time_policy" \
-		"max_inactive_time" \
-		"max_inactive_time_policy")
-	for attr in "${attrs[@]}"; do
-		if [ ! -f "${dir}/${attr}" ]; then
-			exit_failed " --> FAILED (missing ${attr} for ${rw} descriptor $2)"
-		fi
-	done
-}
-
-# Check base directory and its content
-sysfsdir="/sys/block/${bdev}/device/duration_limits"
-
-echo "Checking ${sysfsdir}"
-
-if [ ! -d "${sysfsdir}" ]; then
-	exit_failed " --> FAILED (no duration_limits sysfs device directory)"
+if [ ! -f "${sysfsdir}/cdl_supported" ]; then
+	exit_failed " --> FAILED (missing cdl_supported attribute)"
 fi
 
-if [ ! -f "${sysfsdir}/enable" ]; then
-	exit_failed " --> FAILED (missing enable attribute)"
-fi
+echo "Checking ${sysfsdir}/cdl_enable"
 
-if [ ! -f "${sysfsdir}/perf_vs_duration_guideline" ]; then
-	exit_failed " --> FAILED (missing perf_vs_duration_guideline attribute)"
+if [ ! -f "${sysfsdir}/cdl_enable" ]; then
+	exit_failed " --> FAILED (missing cdl_enable attribute)"
 fi
-
-for rw in read write; do
-	# Check descriptor pages and their descriptors
-	rwdir="${sysfsdir}/${rw}"
-	check_rw "${rwdir}"
-	for desc in 1 2 3 4 5 6 7; do
-		check_desc "${rwdir}" "${desc}"
-	done
-done
 
 exit 0
