28b38975c46650e7c1bf04aa17a1bf663ba1261e
Damien Le Moal <damien.lemoal@opensource.wdc.com>
2023-03-02 09:28:12 +0900

    tests: Fix inactive time non-ncq read tests
    When executing read commands without NCQ enabled, a read command is
    issued to the drive after the completion of the previous command (QD=1
    operation). There is thus never any inactive time for the command as it
    is executed immediately by the device. We should thus not expect any
    ETIME error when using policy 0xd and 0cf. Fix tests 0521 and 0522 to
    not expect IO errors. Also while at it, cleanup the test scripts
    (including 0520 test) to quote variables and avoid long lines.
    
    Signed-off-by: Damien Le Moal <damien.lemoal@opensource.wdc.com>

--- tests/scripts/0521.sh
+++ tests/scripts/0521.sh
@@ -8,13 +8,6 @@
 . "${scriptdir}/test_lib"
 
 testname="CDL inactive time (0xd complete-unavailable policy) reads ncq=off"
-T2A_file="${scriptdir}/cdl/T2A-inactive-time.cdl"
-T2B_file="${scriptdir}/cdl/T2B-empty.cdl"
-cdl_dld=2
-expect_error=1
-compare_latencies=0
-ncq=0
-rw=randread
 
 if [ $# == 0 ]; then
 	echo $testname
@@ -24,11 +17,28 @@ fi
 filename=$0
 dev=$1
 
+#
+# Note: with NCQ disabled, the drive operates at QD=1 so there should be
+# no inactive time at all for any read command. This test should thus
+# never generate any ETIME error.
+#
+T2A_file="${scriptdir}/cdl/T2A-inactive-time.cdl"
+T2B_file="${scriptdir}/cdl/T2B-empty.cdl"
+cdl_dld=2
+expect_error=0
+compare_latencies=0
+ncq=0
+rw="randread"
+
 if dev_has_bad_fw "$1"; then
 	exit_skip
 fi
 
-execute_test "$testname" $T2A_file $T2B_file $cdl_dld $expect_error $compare_latencies $filename $dev $ncq $rw || \
+execute_test "${testname}" \
+	"${T2A_file}" "${T2B_file}" \
+	"${cdl_dld}" "${expect_error}" \
+	"${compare_latencies}" "${filename}" \
+	"${dev}" "${ncq}" "${rw}" || \
 	exit_failed " --> FAILED (error executing test)"
 
 exit 0
--- tests/scripts/0522.sh
+++ tests/scripts/0522.sh
@@ -8,13 +8,6 @@
 . "${scriptdir}/test_lib"
 
 testname="CDL inactive time (0xf abort policy) reads ncq=off"
-T2A_file="${scriptdir}/cdl/T2A-inactive-time.cdl"
-T2B_file="${scriptdir}/cdl/T2B-empty.cdl"
-cdl_dld=3
-expect_error=1
-compare_latencies=0
-ncq=0
-rw=randread
 
 if [ $# == 0 ]; then
 	echo $testname
@@ -24,7 +17,24 @@ fi
 filename=$0
 dev=$1
 
-execute_test "$testname" $T2A_file $T2B_file $cdl_dld $expect_error $compare_latencies $filename $dev $ncq $rw || \
+#
+# Note: with NCQ disabled, the drive operates at QD=1 so there should be
+# no inactive time at all for any read command. This test should thus
+# never generate any ETIME error.
+#
+T2A_file="${scriptdir}/cdl/T2A-inactive-time.cdl"
+T2B_file="${scriptdir}/cdl/T2B-empty.cdl"
+cdl_dld=3
+expect_error=0
+compare_latencies=0
+ncq=0
+rw="randread"
+
+execute_test "${testname}" \
+	"${T2A_file}" "${T2B_file}" \
+	"${cdl_dld}" "${expect_error}" \
+	"${compare_latencies}" "${filename}" \
+	"${dev}" "${ncq}" "${rw}" || \
 	exit_failed " --> FAILED (error executing test)"
 
 exit 0
