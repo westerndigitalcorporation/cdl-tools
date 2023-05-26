e954f126c68e67d669bf147e816d3267e1aadf9c
Damien Le Moal <dlemoal@kernel.org>
2023-05-26 11:53:26 +0900

    tests: Add active+inactive time limits test cases
    Add the new test cases 0330, 0331, 0332, 0333 and 0334 to exercise the
    test device with read IO workloads using limit desciptors that combine
    the active AND inactive time limits with policies 0x0, 0xd and 0xf.
    
    Signed-off-by: Damien Le Moal <dlemoal@kernel.org>

---
+++ tests/scripts/0330.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# SPDX-License-Identifier: GPL-2.0-or-later
+#
+# Copyright (C) 2023 Western Digital Corporation or its affiliates.
+#
+
+. "${scriptdir}/test_lib"
+
+testname="CDL active (0x0 policy) + inactive (0x0 policy) reads"
+
+if [ $# == 0 ]; then
+	echo $testname
+	exit 0
+fi
+
+filename=$0
+dev=$1
+
+read_limits="combined-time"
+write_limits=""
+cdl_dld=1
+expect_error=0
+compare_latencies=0
+ncq=1
+rw="randread"
+
+execute_test "${testname}" \
+	"${read_limits}" "${write_limits}" \
+	"${cdl_dld}" "${expect_error}" \
+	"${compare_latencies}" "${filename}" \
+	"${dev}" "${ncq}" "${rw}" || \
+	exit_failed "error executing fio test"
+
+exit 0
