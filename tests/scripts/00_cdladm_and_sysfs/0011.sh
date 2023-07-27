0db5261f2f282984dc0e3a3b70297bf9f8d1c5cc
Damien Le Moal <dlemoal@kernel.org>
2023-07-27 18:43:18 +0900

    tests: 0011: Ignore High priority enhancement support
    Since SCSI does not support command priorities, the High priority
    enhancement feature is always reported as not supported for ata devices
    connected to SAS adapters. Make sure to ignore this feature when
    comparing cmdadm show outputs with SCSI and with ATA.
    
    Signed-off-by: Damien Le Moal <dlemoal@kernel.org>

--- tests/scripts/00_cdladm_and_sysfs/0011.sh
+++ tests/scripts/00_cdladm_and_sysfs/0011.sh
@@ -13,35 +13,47 @@ if [ $# == 0 ]; then
 fi
 
 echo "# cdladm show $1"
-cdladm show "$1" | tee /tmp/cdl-show || \
-	exit_failed
+cdladm show "$1" |
+	grep -v "High priority enhancement:" | \
+	tee /tmp/cdl-show || \
+		exit_failed
 
 echo "# cdladm show --page T2A $1"
-cdladm show --page T2A "$1" | tee /tmp/cdl-show-t2a || \
-	exit_failed
+cdladm show --page T2A "$1" |
+	grep -v "High priority enhancement:" | \
+	tee /tmp/cdl-show-t2a || \
+		exit_failed
 
 echo "# cdladm show --page T2B $1"
-cdladm show --page T2B "$1" | tee /tmp/cdl-show-t2b || \
-	exit_failed
+cdladm show --page T2B "$1" |
+	grep -v "High priority enhancement:" | \
+	tee /tmp/cdl-show-t2b || \
+		exit_failed
 
 if dev_is_ata "$1"; then
 	echo "# cdladm show --force-ata $1"
-	cdladm show --force-ata "$1" | tee /tmp/cdl-show-ata || \
-		exit_failed
+	cdladm show --force-ata "$1" |
+		grep -v "High priority enhancement:" | \
+		tee /tmp/cdl-show-ata || \
+			exit_failed
 
 	diff -q  /tmp/cdl-show /tmp/cdl-show-ata || \
 		exit_failed "ata show differs from SAT show"
 
 	echo "# cdladm show --force-ata --page T2A $1"
-	cdladm show --force-ata --page T2A "$1" | tee /tmp/cdl-show-t2a-ata || \
-		exit_failed
+	cdladm show --force-ata --page T2A "$1" |
+		grep -v "High priority enhancement:" | \
+		tee /tmp/cdl-show-t2a-ata || \
+			exit_failed
 
 	diff -q  /tmp/cdl-show-t2a /tmp/cdl-show-t2a-ata || \
 		exit_failed "T2A ata show differs from SAT show"
 
 	echo "# cdladm show --force-ata --page T2B $1"
-	cdladm show --force-ata --page T2B "$1" | tee /tmp/cdl-show-t2b-ata || \
-		exit_failed
+	cdladm show --force-ata --page T2B "$1" |
+		grep -v "High priority enhancement:" | \
+		tee /tmp/cdl-show-t2b-ata || \
+			exit_failed
 
 	diff -q  /tmp/cdl-show-t2b /tmp/cdl-show-t2b-ata || \
 		exit_failed "T2B ata show differs from SAT show"
